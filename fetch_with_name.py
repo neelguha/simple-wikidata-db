a758a0b5214f38e950f0c8bed8b631f02e3524c3
Neel Guha <nguha@raiders7.stanford.edu>
2020-12-17 13:09:03 -0800

    First commit of code

---
+++ fetch_with_name.py
@@ -0,0 +1,55 @@
+"""
+This script fetches all QIDs which are associated with a particular name/alias (i.e. "Victoria")
+
+to run: 
+python3.6 fetch_aliases.py --data $DATA --out_dir $OUT --qid Q30
+""" 
+
+    
+import os, json, argparse, time, shutil
+from tqdm import tqdm 
+from multiprocessing import Pool
+from functools import partial 
+
+from utils import *
+
+def get_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type = str, default = 'data/processed/aliases', help = 'path to output directory')
+    parser.add_argument('--name', type = str, default='Victoria', help ='name to search for')
+    return parser 
+
+
+def filtering_func(target_name, filename):
+    filtered = []
+    for item in jsonl_generator(filename):
+        if item['alias'] == target_name:
+            filtered.append(item)
+    return filtered
+
+def main():
+    start = time.time()
+    args = get_arg_parser().parse_args()
+
+    table_files = get_batch_files(args.data)
+    pool = Pool(processes = 10)
+    filtered = []
+    for output in tqdm(
+        pool.imap_unordered(
+            partial(filtering_func, args.name), table_files, chunksize=1), 
+        total=len(table_files)
+    ):
+        filtered.extend(output)
+    
+    print(f"Extracted {len(filtered)} rows:")
+    for i, item in enumerate(filtered):
+        print(f"Row {i}: {item}")
+    
+
+                
+    
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ fetch_with_rel_and_value.py
@@ -0,0 +1,58 @@
+"""
+This script fetches all QIDs which have a relationship with a specific value. 
+
+For example: all entities which played 'quarterback' on a football team (corresponding to P413 and a value of Q622747)
+
+to run: 
+python3.6 fetch_with_rel_and_value.py --data $DATA --out_dir $OUT
+""" 
+
+    
+import os, json, argparse, time, shutil
+from tqdm import tqdm 
+from multiprocessing import Pool
+from functools import partial 
+
+from utils import *
+
+def get_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type = str, default = 'data/processed/entity_rels', help = 'path to output directory')
+    parser.add_argument('--rel', type = str, default='P413', help ='relationship')
+    parser.add_argument('--entity', type = str, default='Q622747', help ='entity value')
+    return parser 
+
+
+def filtering_func(rel, entity, filename):
+    filtered = []
+    for item in jsonl_generator(filename):
+        if item['property_id'] == rel and item['value'] == entity:
+            filtered.append(item)
+    return filtered
+
+def main():
+    start = time.time()
+    args = get_arg_parser().parse_args()
+
+    table_files = get_batch_files(args.data)
+    pool = Pool(processes = 10)
+    filtered = []
+    for output in tqdm(
+        pool.imap_unordered(
+            partial(filtering_func, args.rel, args.entity), table_files, chunksize=1), 
+        total=len(table_files)
+    ):
+        filtered.extend(output)
+    
+    print(f"Extracted {len(filtered)} rows:")
+    for i, item in enumerate(filtered):
+        print(f"Row {i}: {item}")
+    
+
+                
+    
+    
+
+
+if __name__ == "__main__":
+    main()
